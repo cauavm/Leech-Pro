064938895018ab3074eac622b5fd3f5e8b9c53c4
cauavm <cauavm@users.noreply.github.com>
2022-01-12 17:19:35 -0300

    traduções 3/4

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -15,7 +15,7 @@ async def new_join_f(client, message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('Channel', url='https://t.me/MaxxBots')
+                        InlineKeyboardButton('Channel', url='https://t.me/animezey')
                     ]
                 ]
                )
@@ -32,27 +32,27 @@ async def help_message_f(client, message):
     else:
         utxt = "Streamable"
     await message.reply_text(
-        f"""Available Commands
+        f"""Comandos disponíveis
 /{RCLONE_COMMAND} : This will change your drive config on fly.(First one will be default)
  
 /{CLONE_COMMAND_G}: This command is used to clone gdrive files or folder using gclone.
 Syntax:- `[ID of the file or folder][one space][name of your folder only(If the id is of file, don't put anything)]` and then reply /gclone to it.
  
-/{LOG_COMMAND}: This will send you a txt file of the logs.
+/{LOG_COMMAND}: Gerar logs.
  
-/{YTDL_COMMAND}: This command should be used as reply to a supported link
+/{YTDL_COMMAND}: Este comando deve ser usado em resposta a um link suportado.
  
-/{PYTDL_COMMAND}: This command will download videos from youtube playlist link and will upload to telegram.
+/{PYTDL_COMMAND}: Baixar vídeos de uma playlist do YouTube e fazer o upload para o Telegram.
  
-/{GYTDL_COMMAND}: This will download and upload to your cloud.
+/{GYTDL_COMMAND}: Baixar e fazer o upload para o Google Drive.
  
-/{GPYTDL_COMMAND}: This download youtube playlist and upload to your cloud.
+/{GPYTDL_COMMAND}: Baixar playlist do YouTube e fazer o upload para o Google Drive.
  
-/{LEECH_COMMAND}: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [this command will SPAM the chat and send the downloads a seperate files, if there is more than one file, in the specified torrent]
+/{LEECH_COMMAND}: Este comando deve ser usado em resposta a um Magnet Link, link de Torrent, ou link direto. [Irá gerar SPAM no chat e enviar os downloads como arquivos separados, se houver mais de um arquivo.].
  
-/{LEECH_ZIP_COMMAND}: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [This command will create a .tar.gz file of the output directory, and send the files in the chat, splited into PARTS of 1024MiB each, due to Telegram limitations]
+/{LEECH_ZIP_COMMAND}: Este comando deve ser usaddo em resposta a um Magnet Link, link de Torrent, ou link direto. [Irá criar um arquivo .tar.gz na saída do diretório e enviar os arquivos no chat, dividido em partes de 1024MiB cada, devido a limitações do Telegram]
  
-/{GLEECH_COMMAND}: This command should be used as reply to a magnetic link, a torrent link, or a direct link. And this will download the files from the given link or torrent and will upload to the cloud using rclone.
+/{GLEECH_COMMAND}: Este comando deve ser usado em resposta a um Magnet Link, link de Torrent, ou link direto. Irá baixar os arquivos do link ou torrent enviado e irá fazer o upload para o Google Drive usando o Rclone.
  
 /{GLEECH_ZIP_COMMAND} Este comando irá comprimir a pasta/arquivo e fazer o upload para o Google Drive.
  
@@ -60,17 +60,17 @@ Syntax:- `[ID of the file or folder][one space][name of your folder only(If the 
  
 /{GLEECH_UNZIP_COMMAND}: Descompactar o arquivo e fazer o upload para o Google Drive.
  
-/{TELEGRAM_LEECH_COMMAND}: This will mirror the telegram files to ur respective cloud .
+/{TELEGRAM_LEECH_COMMAND}: Fazer um Mirror do arquivo do Telegram para o Google Drive.
  
-/{TELEGRAM_LEECH_UNZIP_COMMAND}: This will unarchive telegram file and upload to cloud.
+/{TELEGRAM_LEECH_UNZIP_COMMAND}: Descompactar arquivo do Telegram e fazer o upload para o Google Drive.
  
-/{GET_SIZE_G}: This will give you total size of your destination folder in cloud.
+/{GET_SIZE_G}: Mostrar o tamanho total da pasta no Google Drive.
  
-/{RENEWME_COMMAND}: This will clear the remains of downloads which are not getting deleted after upload of the file or after /cancel command.
+/{RENEWME_COMMAND}: Deletar os downloads restantes que não estão sendo deletados após o arquivo ser uplado ou após o comando /cancel
  
-/{CANCEL_COMMAND_G} [GID]: To cancel ur download
+/{CANCEL_COMMAND_G} [GID]: para cancelar o seu download.
 
-/{RENAME_COMMAND}: To rename the telegram files.
+/{RENAME_COMMAND}: Renomear arquivos do Telegram.
  
 Only work with direct link and youtube link for nowIt is like u can add custom name as prefix of the original file name. Like if your file name is gk.txt uploaded will be what u add in CUSTOM_FILE_NAME + gk.txt
  
@@ -82,13 +82,13 @@ You have to pass link as www.download.me/gk.txt | new.txt
  
 the file will be uploaded as new.txt.
  
-/{SAVE_THUMBNAIL}: Reply To A Photo To Save As Custom Thumbnail
+/{SAVE_THUMBNAIL}: Utilize em resposta a uma Foto para salvar como uma Thumbnail Personalizada.
 
-/{CLEAR_THUMBNAIL}: To Clear Saved Custom Thumbnail
+/{CLEAR_THUMBNAIL}: Limpar Thumbnails Personalizadas salvas.
 
-/{TOGGLE_VID}: To Upload Your Files As Streamable
+/{TOGGLE_VID}: Fazer o upload do arquivo como Streamável
 
-/{TOGGLE_DOC}: To Upload Your Files As Documents
+/{TOGGLE_DOC}: Fazer o upload do arquivo como Documento
 
 **How to Use....?**
 __Send any one of the available command, as a reply to a valid link/magnet/torrent. 👊__
